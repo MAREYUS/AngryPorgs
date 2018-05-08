c9177dbad3cdbe5fde7b9b5d7bd5100e6bfc3605
Marius Schmücker <marius_schmuecker@outlook.com>
2018-05-08 22:02:05 +0200

    Added GUI + Game Manager + Goals

---
+++ Assets/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	
+    public enum GameState{
+        Active,
+        Won,
+        Lost
+    }
+
+
+    private int goalAmount;
+
+    private void Start()
+    {
+        goalAmount = GameObject.Find("Goals").transform.childCount;
+        print("Goal Amount: " + goalAmount);
+    }
+
+
+    // restart the Game
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // load next Level
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // subscribe to GoalHit
+    private void OnEnable()
+    {
+        Goal.GoalHitEvent += GoalHit;
+    }
+
+    public void GoalHit()
+    {
+        print("Goal Hit! (GameManager)");
+    }
+
+}
--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -6,6 +6,11 @@ public class Goal : MonoBehaviour {
 	// A static field visible from anywhere
 	public static bool goalMet = false;
 
+    public delegate void GoalHit();
+    public static event GoalHit GoalHitEvent;
+
+    
+
 	void OnTriggerEnter2D(Collider2D other) {
 		// Check if the hit comes from a projectile
 		if(other.gameObject.tag == "Missile") {
@@ -16,6 +21,9 @@ public class Goal : MonoBehaviour {
             c.a = 1.0f;
             GetComponent<SpriteRenderer>().material.color = c;
             GetComponent<Collider2D>().enabled = false;
+
+            if (GoalHitEvent != null)
+                GoalHitEvent();
         }
 	}
 }
