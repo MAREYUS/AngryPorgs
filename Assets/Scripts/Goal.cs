1ff9ddcdb206a8a94c747701b676774cbbddf301
Marius Schmücker <marius_schmuecker@outlook.com>
2018-05-19 00:53:13 +0200

    Replaced Triggers by Collision + Added destroyable Obstacles

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -8,18 +8,37 @@ public class Goal : MonoBehaviour {
     
     public UnityEvent GoalHitEvent; 
     
-    void OnTriggerEnter2D(Collider2D other) {
-		// Check if the hit comes from a projectile
-		if(other.gameObject.tag == "Missile") {
+ //   void OnTriggerEnter2D(Collider2D other) {
+	//	// Check if the hit comes from a projectile
+	//	if(other.gameObject.tag == "Missile") {
+ //           print("Goal Hit (Goal)");
+ //           // Set alpha to higher opacity
+ //           Color c = GetComponent<SpriteRenderer>().material.color;
+ //           c = new Color(0f, 1f, 0f, 1f);
+ //           GetComponent<SpriteRenderer>().material.color = c;
+ //           GetComponent<Collider2D>().enabled = false;
+           
+ //           GoalHitEvent.Invoke();
+ //       }
+	//}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Missile")
+        {
             print("Goal Hit (Goal)");
             // Set alpha to higher opacity
             Color c = GetComponent<SpriteRenderer>().material.color;
             c = new Color(0f, 1f, 0f, 1f);
             GetComponent<SpriteRenderer>().material.color = c;
+            if(GetComponent<SineMovement>() != null)
+                GetComponent<SineMovement>().enabled = false;
+            GetComponent<Rigidbody2D>().gravityScale = 1.0f;
             GetComponent<Collider2D>().enabled = false;
-           
+
+
             GoalHitEvent.Invoke();
         }
-	}
+    }
 
 }
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    public FloatReference maxHP;
+    public FloatReference collisionForceFactor;
+
+    private float hP;
+
+    private void Awake()
+    {
+        hP = maxHP.Value;
+    }
+
+    private void Update()
+    {
+        if (hP <= 0)
+            Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+            hP = hP - (collision.relativeVelocity.magnitude * collisionForceFactor);
+        
+    }
+}
